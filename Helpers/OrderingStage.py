99f3b86ecc5a33b09829a8fd16fb0c02ef547302
MerlinLim <merlinlim98@gmail.com>
2020-05-31 11:50:09 +0800

    addOrder and viewOrder now implements telegram methods.
    addOrder reads menu and asks customer what to order using an inline replymarkup.
    Method creates a lower level CallBackQueryHandler to handle replies.
    This handler invokes addOrder_helper to handle replies
    
    addOrder_helper adds item to list of items using updateList method
    and prompts user which item they have ordered.
    
    viewOrder sends output to chat as a message.

--- Helpers/OrderingStage.py
+++ Helpers/OrderingStage.py
@@ -2,34 +2,62 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Callback
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InlineQuery
 
 class order:
-    def __init__(self, user, restuarant=None, address = None, phone = None):
+    def __init__(self, user, restuarant = None, address = None, phone = None):
         self.user = user
         self.restuarant = restuarant
         self.address = address
         self.phone = phone
         self.food = {}
 
-    def addOrder(self, name, item):
+    def addOrder(self, update, context):
+        #Access Menu
+        order_list = menu()
+
+        # Create and send menu.
+        # Header: Prompt user to select food/drink.
+        reply_markup = self.InlineKeyboard(order_list)
+        context.bot.sendMessage(chat_id = update.effective_user.id,
+                                    text = "Please select your food/drink:", reply_markup = reply_markup)
+
+        #handle answer using addOrder_helper
+        answer_handler = CallbackQueryHandler(lambda update, context: addOrder_helper(update, context))
+        context.dispatcher.add_handler(answer_handler)
+
+    def addOrder_helper(self, update, context):
+        #check for if person has answered
+        if(context.bot.answer_callback_query(update.callback_query.id)):
+        
+            #remove message text
+            context.bot.deleteMessage(update.effective_chat.id, update.callback_query.message.message_id)
+            
+            #send new message
+            item = update.callback_query.data
+            context.bot.sendMessage(chat_id = update.effective_user.id,
+                                    text = "You have ordered " + item)
+
+            #save order
+            customer = update.effective_user
+            updateList(customer, item)
+
+    def updateList(self, customer, item):
         #check if name exists
-        if name in self.food:
-            temp = self.food[name]
+        if customer in self.food:
+            temp = self.food[customer]
             #check if item exists
             if item in temp:
                 temp[item] = temp[item] + 1
             else:
                 temp[item] = 1
         else:
-            self.food[name] = {name:1}
+            self.food[name] = {customer:1}
     
-    
-    def viewOrder(self):
+    def viewOrder(self, update, context):
         output = ""
-        for item in self.food :
-            temp = self.food[item]
-            for name in temp :
-                output = output + name + " " + item + " x" + temp[name] + "\n"
-        return output
-
+        for customer in self.food :
+            temp = self.food[customer]
+            for item in temp :
+                output = output + customer.first_name + " " + customer.last_name + " " + item + " x" + temp[item] + "\n"
+        context.bot.sendMessage(chat_id = update.effective_chat.id, text = output)
 
     def deleteOrder(self, update, context):
         #step 1: List items chosen
@@ -39,6 +67,7 @@ class order:
         if list_of_items is None:
             context.bot.sendMessage(chat_id = update.effective_user.id, text = "You have not ordered anything yet")
         else:
+            # Create and send menu
             reply_markup = self.InlineKeyboard(list_of_items)
             context.bot.sendMessage(chat_id  = update.effect_user.id, text = "Select which item to delete", reply_markup = reply_markup)
         
@@ -62,20 +91,19 @@ class order:
                 del self.food[user]
             
             #send new message
-            context.bot.sendMessage(chat_id=update.effective_user.id,
-                                text= item + " has been deleted!" )
+            context.bot.sendMessage(chat_id = update.effective_user.id,
+                                text = item + " has been deleted!" )
 
     def build_menu(self, buttons,
                n_cols,	
-               header_buttons=None,
-               footer_buttons=None):
+               header_buttons = None,
+               footer_buttons = None):
 	menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
 	if header_buttons:
 		menu.insert(0, [header_buttons])
 	if footer_buttons:
 		menu.append([footer_buttons])
 	return menu
-
 
     def InlineKeyboard(self, list_of_options):
         keyboard = [InlineKeyboardButton(item[1], callback_data = item[1]) for item in list_of_options]
