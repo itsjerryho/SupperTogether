387591db5ca616d94f1b6f35abfe3554e223c957
MerlinLim <merlinlim98@gmail.com>
2020-06-24 01:37:32 +0800

    Adds Conversationhandlers for callback queries
    ConversationHandlers map the CallbackQueryHandlers to their respective Commandhandlers using states.
    addOrder and removeOrder now make use of this to manage callbackquery data from user responses.
    
    sub_1, sub_2, sub_3 are states refering to secondary stages Conversation.
    
    Method build_handler builds ConversationHandlers for addOrder and removeOrder methods, while
    viewOrder method is as a CommandHandler. build_handler takes in a dispatcher type argument and
    adds the handlers to the dispatcher.
    
    Following Main.py, deleteOrder renamed to removeOrder.
    
    Rest_menu returns lists food items in the new format [itemname1, itemname2.....]

--- Helpers/OrderingStage.py
+++ Helpers/OrderingStage.py
@@ -1,6 +1,8 @@
 from openpyxl import load_workbook, workbook
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, ConversationHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InlineQuery
+
+sub_1, sub_2, sub_3 = range(3)
 
 class order:
     def __init__(self, user, restuarant = None, address = None, phone = None):
@@ -14,15 +16,14 @@ class order:
         #Access Menu
         order_list = self.rest_menu()
 
-        # Create and send menu.
-        # Header: Prompt user to select food/drink.
+        # Create reply_markup.
         reply_markup = self.InlineKeyboard(order_list)
+        
+        # Header: Prompt user to select food/drink.
         context.bot.sendMessage(chat_id = update.effective_user.id,
                                     text = "Please select your food/drink:", reply_markup = reply_markup)
 
-        #handle answer using addOrder_helper
-        answer_handler = CallbackQueryHandler(lambda update, context: addOrder_helper(update, context))
-        context.dispatcher.add_handler(addOrder_helper)
+        return sub_1
 
     def addOrder_helper(self, update, context):
         #check for if person has answered
@@ -39,6 +40,8 @@ class order:
             #save order
             customer = update.effective_user
             updateList(customer, item)
+            
+            return ConversationHandler.END
 
     def updateList(self, customer, item):
         #check if name exists
@@ -60,24 +63,24 @@ class order:
                 output = output + customer.first_name + " " + customer.last_name + " " + item + " x" + temp[item] + "\n"
         context.bot.sendMessage(chat_id = update.effective_chat.id, text = output)
 
-    def deleteOrder(self, update, context):
+    def removeOrder(self, update, context):
         #step 1: List items chosen
         list_of_items = self.food[update.effective_user]
         
         #step 2: choose from menu
         if list_of_items is None:
             context.bot.sendMessage(chat_id = update.effective_user.id, text = "You have not ordered anything yet")
+            
+            return ConversationHandler.END
+        
         else:
             # Create and send menu
             reply_markup = self.InlineKeyboard(list_of_items)
             context.bot.sendMessage(chat_id  = update.effect_user.id, text = "Select which item to delete", reply_markup = reply_markup)
-        
-        #step 3: delete item
-            deleteHelper_handler = CallbackQueryHandler(lambda update, context: deleteHelper(self, update, context))
-            
-            context.dispatcher.add_handler(deleteHelper_handler)
 
-    def deleteHelper(self, update, context):
+            return sub_1
+        
+    def removeOrder_Helper(self, update, context):
         if(context.bot.answer_callback_query(update.callback_query.id)):
             #remove message text
             context.bot.deleteMessage(update.effective_chat.id, update.callback_query.message.message_id)
@@ -94,6 +97,8 @@ class order:
             #send new message
             context.bot.sendMessage(chat_id = update.effective_user.id,
                                 text = item + " has been deleted!" )
+            
+            return ConversationHandler.END
 
     def build_menu(self, buttons,
                n_cols,	
@@ -107,17 +112,29 @@ class order:
 	return menu
 
     def InlineKeyboard(self, list_of_options):
-        keyboard = [InlineKeyboardButton(item[1], callback_data = item[1]) for item in list_of_options]
+        keyboard = [InlineKeyboardButton(item, callback_data = item) for item in list_of_options]
         return InlineKeyboardMarkup(build_menu(keyboard, n_cols = 1))
 
     def rest_menu(self):
         #Menu
         Menu_wb = load_workbook(filename = 'Menu.xlsx')
         restaurant_ws = Menu_wb ['RESTAURANT A']
-        ORDERID = restaurant_ws['A']
         ITEMNAME = restaurant_ws['B']
 
         #return list of orders[ORDERID, ITEMNAME]
-        return [ [ORDERID[i].value, ITEMNAME[i].value] for i in range(0, len(ORDERID)) ]
-        
+        return [ITEMNAME[i].value for i in range(0, len(ITEMNAME)) ]
+    
+    def build_handlers(self, dispatcher):
+        order_conv_handler = ConversationHandler(entry_points = [CommandHandler("AddOrder", self.addOrder)], states = {
+            sub_1 : [CallbackQueryHandler(self.addOrder_helper)]
+        }, fallbacks = [CommandHandler("AddOrder", self.addOrder)], per_user = True, per_chat = False)
 
+        remove_conv_handler = ConversationHandler(entry_points = [CommandHandler("RemoveOrder", self.removeOrder)], states = {
+            sub_1 : [CallbackQueryHandler(self.removeOrder_Helper)]
+        }, fallbacks = [CommandHandler("RemoveOrder", self.removeOrder)], per_user = True, per_chat = False)
+
+        viewOrder_handler = CommandHandler("vieworder", self.viewOrder)
+
+        dispatcher.add_handler(order_conv_handler)
+        dispatcher.add_handler(remove_conv_handler)
+        dispatcher.add_handler(viewOrder_handler)
