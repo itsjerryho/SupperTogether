731b8bbe09ed8a52495b5878e5fc9487657c1760
MerlinLim <merlinlim98@gmail.com>
2020-06-24 03:07:20 +0800

    Adds logger, start command and modifies handlers.
    Logger and error handler used for bot testing.
    
    Replaces startJio with start_conv, removes addOrder, removeOrder and viewOrder commands
    as preOrdering stage adds them later.
    
    Start command simply prompts users on how to get started.

--- Main.py
+++ Main.py
@@ -1,24 +1,36 @@
-# import function name 
-from Helpers import StartJio, StoreMode, AddOrder, ViewOrder, RemoveOrder, CloseJio
+# import
+from telegram.ext import Updater, CommandHandler
+from Helpers.PreOrderingStage import start_conv, CloseJio
+from Helpers.StoreInterface import StoreMode
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+#Loggin and errors
+logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+def error(update, context):
+    #Log Errors caused by Updates.
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
     updater = Updater("1054268461:AAFSMepDnv3L4bq7bs9_ykhrnZVt24whpHU", use_context=True)
 
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler(“StartJio”, StartJio)
-	dp.add_handler(CommandHandler(“Store”, StoreMode)
-	dp.add_handler(CommandHandler(“AddOrder”, AddOrder)
-    dp.add_handler(CommandHandler(“ViewOrder”, ViewOrder)
-    dp.add_handler(CommandHandler(“RemoveOrder”, RemoveOrder)
-    dp.add_handler(CommandHandler(“CloseJio”, CloseJio)
+    dp.add_handler(start_conv)
+    dp.add_handler(CommandHandler('Store', StoreMode))
+    dp.add_handler(CommandHandler('CloseJio', CloseJio))
+    dp.add_error_handler(error)
     # dp.add_handler(CommandHandler(“Help”, help)
 
     # start checking for updates
     updater.start_polling()
 
     updater.idle()
+
+def Start(update, context):
+    #Prompt user to startJio
+    context.bot.sendMessage(chat_id = update.effective_chat.id, text = "Hi, weclome to SupperTogether! To start jio people, just /StartJio")
 
 if __name__ == '__main__':
     main()
