3f1141f9e651fe19e507890c1063ef19ccd22bc0
MerlinLim <merlinlim98@gmail.com>
2020-07-08 02:54:15 +0800

    Adds secondary options to Addorder method
    If there are secondary options present for the chosen item in Addorder,
    the bot will edit the existing message and change the reply_markup
    to show new secondary options to choose from. The current number of
    secondary options is 2 for simplicity sake.
    
    Addorder conversation handler is builds with options. Options is called
    twice or less depending on the number of options. When there are no more
    options, addorder_helper is called to combine all the option names and
    prices.
    
    Dictionaries of food in order objects uses tuples as keys instead of
    strings to accomadate for the options. To updateList and removefood,
    the ID should be of type tuple.
    
    Fixes removeOrder. Bot prompts user to that he has not added anything
    if removeOrder is called when the user has not ordered yet.

--- Helpers/OrderingStage.py
+++ Helpers/OrderingStage.py
@@ -4,6 +4,7 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Callback
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InlineQuery
 
 sub_1, sub_2, sub_3 = range(3)
+option_1, option_2, final = range(3)
 
 class Order:
     def __init__(self, user, restaurant = None, address = None, phone = None):
@@ -49,7 +50,9 @@ class Order:
     
 def addOrderHandlersTo(dispatcher):
     order_conv_handler = ConversationHandler(entry_points = [CommandHandler("AddOrder", addOrder)], states = {
-        sub_1 : [CallbackQueryHandler(addOrder_helper)]
+        option_1 : [CallbackQueryHandler(lambda update, context: options(update, context, option_1))],
+        option_2 : [CallbackQueryHandler(lambda update, context: options(update, context, option_2))],
+        final : [CallbackQueryHandler(lambda update, context: addOrder_helper(update, context, final))]
     }, fallbacks = [CommandHandler("AddOrder", addOrder)], per_user = True, per_chat = False)
 
     remove_conv_handler = ConversationHandler(entry_points = [CommandHandler("RemoveOrder", removeOrder)], states = {
@@ -86,39 +89,73 @@ def addOrder(update, context):
 
     # Create reply_markup.
     reply_markup = InlineKeyboardMarkup(buttons)
+
+    # Cache answers inside user_data["order"]
+    context.user_data["order"] = [None]*3
     
     # Header: Prompt user to select food/drink.
     context.bot.sendMessage(chat_id = update.effective_user.id,
                                 text = "Please select your food/drink:", reply_markup = reply_markup)
 
-    return sub_1
+    return option_1
 
-def addOrder_helper(update, context):
+def options(update, context, option):
+    # Retrieve answer
+    query = update.callback_query
+    data = query.data
+
+    # Save answer
+    context.user_data["order"][option] = data
+
+    # Obtain ID
+    ID = context.user_data["order"][0]
+
+    # Ask next option
+    order = context.bot_data[context.user_data["chat_id"]]
+    list_of_choices = menu.item(order.restaurant, ID , option + 1)
+    list_of_costs = menu.cost(order.restaurant, ID, option + 1)
+
+    # Check if next option exists
+    if list_of_choices is None:
+        return addOrder_helper(update, context, option)
+
+    # Build reply_markup
+    buttons = ([[InlineKeyboardButton(list_of_choices[i] + " - $" + "{:.2f}".format(list_of_costs[i]), callback_data=i)] for i in range(len(list_of_choices))])
+    reply_markup = InlineKeyboardMarkup(buttons)
+
+    # Edit message
+    update.effective_message.edit_text(text = "Please select an option: ",reply_markup = reply_markup)
+
+    return option + 1
+
+def addOrder_helper(update, context, option):
     query = update.callback_query
     query.answer()
     
     chat_id = context.user_data["chat_id"]
-    context.user_data.clear()
     order = context.bot_data[chat_id]
     restaurant = order.restaurant
 
-    # Answer
+    # save answer
     answer = query.data
+    context.user_data["order"][option] = answer
 
     #remove message text
     context.bot.deleteMessage(update.effective_chat.id, update.callback_query.message.message_id)
     
     #send new message
-    item = menu.item(restaurant, answer)
-    amount = menu.cost(restaurant, answer)
+    item = menu.item(restaurant, tuple(context.user_data["order"]))
+    amount = menu.cost(restaurant, tuple(context.user_data["order"]))
     context.bot.sendMessage(chat_id = update.effective_user.id,
                             text = "You have ordered " + item + 
                             "\nAmount: $" + "{:.2f}".format(amount))
 
     #save order
     customer = update.effective_user
-    order.updateList(customer, answer)
-    
+    order.updateList(customer, tuple(context.user_data["order"]))
+
+    # Clear cache
+    context.user_data.clear()
     return ConversationHandler.END
 
 def viewOrder(update, context):
@@ -151,20 +188,21 @@ def removeOrder(update, context):
     context.user_data["chat_id"] = chat_id
     order = context.bot_data[chat_id]
     food = order.food
-
-    #step 1: List items chosen
-    list_of_items = list(food[update.effective_user].keys())
-    restaurant = order.restaurant
     
-    #step 2: choose from menu
-    if list_of_items is None:
+    #step 1: check if user has ordered anything
+    if update.effective_user not in food:
         context.bot.sendMessage(chat_id = update.effective_user.id, text = "You have not ordered anything yet")
         
         return ConversationHandler.END
     
-    else:
+    else:        
+        #step 2: List items chosen
+        list_of_items = food[update.effective_user].keys()
+        
         # Create and send menu
-        buttons = [[InlineKeyboardButton(menu.item(restaurant, id), callback_data= id)] for id in list_of_items]
+        restaurant = order.restaurant
+        context.user_data["orders"] = list(list_of_items)
+        buttons = [[InlineKeyboardButton(menu.item(restaurant, context.user_data["orders"][i]), callback_data= i)] for i in range(len(context.user_data["orders"]))]
         reply_markup = InlineKeyboardMarkup(buttons)
         context.bot.sendMessage(chat_id  = update.effective_user.id, text = "Select which item to delete", reply_markup = reply_markup)
 
@@ -173,14 +211,13 @@ def removeOrder(update, context):
 def removeOrder_Helper(update, context):
     # Check chat_id
     chat_id = context.user_data["chat_id"]
-    context.user_data.clear()
 
     # Retrieve order and answers
     order = context.bot_data[chat_id] 
     user = update.effective_user
     restaurant = order.restaurant
-    id = update.callback_query.data
-    answer = menu.item(restaurant, id)
+    i = int(update.callback_query.data)
+    answer = menu.item(restaurant, context.user_data["orders"][i])
 
     # remove message text
     context.bot.deleteMessage(update.effective_chat.id, update.callback_query.message.message_id)
@@ -190,7 +227,10 @@ def removeOrder_Helper(update, context):
                         text = answer + " has been deleted!" )
     
     
-    order.removefood(user, id)
+    order.removefood(user, context.user_data["orders"][i])
+
+    #clear user data
+    context.user_data.clear()
     
     return ConversationHandler.END
 
